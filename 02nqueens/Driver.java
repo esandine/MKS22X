a60749c9f71e7eb6dd50e2af0a5773f2c6469797
esandine <esandine@stuy.edu>
2016-02-09 21:09:37 -0500

    Made placeQueen work

--- 02nqueens/Driver.java
+++ 02nqueens/Driver.java
@@ -2,5 +2,9 @@ public class Driver{
     public static void main(String[]args){
 	Board b1 = new Board(5);
 	System.out.println(b1);
+	System.out.println(b1.placeQueen(2,3));
+	System.out.println(b1.placeQueen(3,4));
+	System.out.println(b1.placeQueen(0,0));
+	System.out.println(b1);
     }
 }
