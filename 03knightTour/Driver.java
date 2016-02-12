f18b4d838b6025318b446157ff51bc795848a20b
esandine <esandine@stuy.edu>
2016-02-12 12:13:06 -0500

    Added stuff to KnightsBoard NOT WORKING

--- 03knightTour/Driver.java
+++ 03knightTour/Driver.java
@@ -1,6 +1,6 @@
 public class Driver{
     public static void main(String[]args){
-	KnightBoard b1 = new KnightBoard(3);
+	KnightBoard b1 = new KnightBoard(3,17);
 	b1.printSolution();
     }
 }
--- 03knightTour/KnightBoard.java
+++ 03knightTour/KnightBoard.java
@@ -2,8 +2,11 @@ public class KnightBoard{
     private int[][] board;
     private boolean DEBUG = true;
     private int[] possibleMoves = {2,-1,2,1,1,-2,1,2,-1,-2,-1,1,-2,1,-2,-1};
+    public KnightBoard(int n, int m){
+	board = new int[n][m];
+    }
     public KnightBoard(int n){
-	board = new int[n][n];
+	this(n,n);
     }
     public void printSolution(){
 	String retStr = "Board:";
@@ -15,5 +18,21 @@ public class KnightBoard{
 	    }
 	}
 	System.out.println(retStr);
+    }
+    public boolean solve(){
+	return solve(0,0,0,0);
+    }
+    public boolean solve(int x, int y, int ctr, int try){
+	if(ctr == board.length*board[0].length){
+	    return true;
+	}
+	if(isValidSpot(x+possibleMoves[try*2],y+possibleMoves[try*2+1])){
+	    return solve(x+possibleMoves[try*2],y+possibleMoves[try*2+1]
+    }
+    public boolean isValidSpot(int x, int y){
+	if((x>board.length)||(y>board[0].length)||(board[x,y]!=0)){
+	    return false;
+	}
+	return true;
     }
 }
