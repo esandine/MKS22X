932c799804f46a46f6882739b651f0b43fa50e77
esandine <esandine@stuy.edu>
2016-02-17 14:40:54 -0500

    Works on a 5x5 board

--- 03knightTour/Driver.java
+++ 03knightTour/Driver.java
@@ -1,6 +1,7 @@
 public class Driver{
     public static void main(String[]args){
-	KnightBoard b1 = new KnightBoard(3,17);
+	KnightBoard b1 = new KnightBoard(5);
+	System.out.println(b1.solve());
 	b1.printSolution();
     }
 }
--- 03knightTour/KnightBoard.java
+++ 03knightTour/KnightBoard.java
@@ -1,7 +1,7 @@
 public class KnightBoard{
     private int[][] board;
     private boolean DEBUG = true;
-    private int[] possibleMoves = {2,-1,2,1,1,-2,1,2,-1,-2,-1,1,-2,1,-2,-1};
+    private int[] possibleMoves = {2,-1,2,1,1,-2,1,2,-1,-2,-1,2,-2,1,-2,-1};
     public KnightBoard(int n, int m){
 	board = new int[n][m];
     }
@@ -14,23 +14,46 @@ public class KnightBoard{
 	    retStr += "\n";
 	    for(int col = 0;col<board[row].length;col++){
 		retStr += " ";
+		if((board[row][col]<10)&&(board.length*board[0].length>10)){
+		    retStr+="_";
+		}
 		retStr+=board[row][col];
 	    }
 	}
 	System.out.println(retStr);
     }
     public boolean solve(){
-	return solve(0,0,0,0);
+	return solveIter(0,0,0,0);
     }
-    public boolean solve(int x, int y, int ctr, int try){
-	if(ctr == board.length*board[0].length){
+    public void revert(int n){
+	for(int row = 0;row<board.length;row++){
+	    for(int col = 0;col<board.length;col++){
+		if(board[row][col]>=n){
+		    board[row][col]=0;
+		}
+	    }
+	}
+    }
+    public boolean solveIter(int x, int y, int ctr, int iter){
+	revert(ctr);
+	if(ctr == board.length*board[0].length+1){
 	    return true;
 	}
-	if(isValidSpot(x+possibleMoves[try*2],y+possibleMoves[try*2+1])){
-	    return solve(x+possibleMoves[try*2],y+possibleMoves[try*2+1]
+	if(isValidSpot(x+possibleMoves[iter*2],y+possibleMoves[iter*2+1])){
+	    board[x+possibleMoves[iter*2]][y+possibleMoves[iter*2+1]]=ctr;
+	    //printSolution();
+	    if(iter<7){
+		return solveIter(x+possibleMoves[iter*2],y+possibleMoves[iter*2+1],ctr+1,0)||solveIter(x,y,ctr,iter+1);
+	    }
+	    return solveIter(x+possibleMoves[iter*2],y+possibleMoves[iter*2+1],ctr+1,0);
+	}
+	if(iter<7){
+	    return solveIter(x,y,ctr,iter+1);
+	}
+	return false;
     }
     public boolean isValidSpot(int x, int y){
-	if((x>board.length)||(y>board[0].length)||(board[x,y]!=0)){
+	if((x>=board.length)||(x<0)||(y>=board[0].length)||(y<0)||(board[x][y]!=0)){
 	    return false;
 	}
 	return true;
