29ec9875ebc3ccd626fad40567035f87c0851283
esandine <esandine@stuy.edu>
2016-02-17 14:50:30 -0500

    Definately works for boards up to 8x8

--- 03knightTour/Driver.java
+++ 03knightTour/Driver.java
@@ -1,5 +1,11 @@
 public class Driver{
     public static void main(String[]args){
+	KnightBoard[] knights = new KnightBoard[8];
+	for(int n = 0;n<8;n++){
+	    knights[n]=new KnightBoard(n+1);
+	    knights[n].solve();
+	    knights[n].printSolution();
+	}
 	KnightBoard b1 = new KnightBoard(5);
 	System.out.println(b1.solve());
 	b1.printSolution();
