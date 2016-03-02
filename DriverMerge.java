fa55f7131d621cf05b1d2490fc1db12937ffb3ea
esandine <esandine@stuy.edu>
2016-03-01 22:08:21 -0500

    Started merging, process is slow

---
+++ DriverMerge.java
@@ -0,0 +1,13 @@
+public class DriverMerge{
+    public static void main(String[]args){
+	int [] data = {6,5,4,3};
+	Sorts.printArray(data);
+	int [] data2 = data;
+	Sorts.merge(data,data2,0,0,1,1);
+	Sorts.printArray(data2);
+	Sorts.merge(data,data2,2,2,3,3);
+	Sorts.printArray(data2);
+	Sorts.merge(data2,data,0,1,2,3);
+	Sorts.printArray(data);
+    }
+}
