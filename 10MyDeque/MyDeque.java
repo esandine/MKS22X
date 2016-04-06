3e47c621ff717ce862c39be0f7ff4fd6bb5ca8bb
esandine <esandine@stuy.edu>
2016-04-05 22:13:34 -0400

    addLast appears to be workign

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -7,5 +7,36 @@ public class MyDeque<T>{
 	public MyDeque(){
 	data=(T[])new Object[10];
     }
-    
+    private int size(){
+	return size;
+    }
+    public void addLast(T value){
+	if(end<data.length-1){
+	    end += 1;
+	}
+	else{
+	    end = 0;
+	}
+	if(size()==0){
+	    start = 0;
+	    end = 0;
+	}
+	data[end]=value;
+	size+=1;
+    }
+    public String toString(){
+	int i = start;
+	String retStr="[";
+	while(i!=end){
+	    retStr+=data[i];
+	    retStr+=",";
+	    if(i==data.length-1){
+		i = 0;
+	    }else{
+		i++;
+	    }
+	}
+	retStr+=data[i];
+	return retStr+"]";
+    }
 }
