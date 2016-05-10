dbe0d80b6bd7fa118504d4bc78a9c4479806392c
esandine <esandine@stuy.edu>
2016-05-10 12:18:49 -0400

    Preminilary tests of getMedian works

---
+++ 14RunningMedians/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[] args){
+	RunningMedian m1 = new RunningMedian();
+	m1.add(2);
+	m1.add(5);
+	m1.add(3);
+	System.out.println(m1.getMedian());
+    }
+}
